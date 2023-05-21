94131468f9f40f88e146715729e22daa2dbebc30
Philip Fourie <philip.fourie@theta.co.nz>
2023-05-21 00:17:37 +0000

    Removed superfluous cd command

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,3 @@ git clone https://github.com/Azure/azure-api-management-devops-resource-kit.git
 
 cd azure-api-management-devops-resource-kit
 dotnet build src/ArmTemplates/ArmTemplates.csproj
-cd ./src/ArmTemplates/bin/Debug/net6.0
